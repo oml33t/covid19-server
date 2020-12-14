cbfba0d9d30a12462821765c39393b2616d9901a
Rahul Singh <dr@ph4nt0m.me>
2020-12-14 13:42:53 +0530

    [fix] accidental gitignore of data

---
+++ src/services/api/v1/data/data.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-mongoose');
+
+exports.Data = class Data extends Service {
+  
+};
---
+++ src/services/api/v1/data/data.service.js
@@ -0,0 +1,18 @@
+// Initializes the `api/v1/data` service on path `/api/v1/data`
+const { Data } = require('./data.class');
+const createModel = require('../../../../models/data.model');
+const hooks = require('./data.hooks');
+
+module.exports = function (app) {
+  const options = {
+    Model: createModel(app)
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/api/v1/data', new Data(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('api/v1/data');
+
+  service.hooks(hooks);
+};
